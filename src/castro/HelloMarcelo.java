c032b1c252970c79c917afaab0bcf4eebcb210a0
Jared Thomasson <jaredthomasson97@gmail.com>
2022-01-29 18:59:33 -0700

    Added a fellow greeting to marcelo

--- src/castro/HelloMarcelo.java
+++ src/castro/HelloMarcelo.java
@@ -3,6 +3,7 @@ package castro;
 public class HelloMarcelo {
 
     public static void main(String[] args) {
-	    System.out.println("Hello, my name is Marcelo");
+        System.out.println("Hello, my name is Marcelo");
+        System.out.println("Hello Marcelo, my name is Jared.");
     }
 }
